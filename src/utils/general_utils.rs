229b8cc0043fd5fd72a65c6680bc700105af95d8
DHCPCD <dhcpcd9@gmail.com>
2024-04-20 19:05:54 +0200

    fix: roundings should be handled better now

--- src/utils/general_utils.rs
+++ src/utils/general_utils.rs
@@ -1,14 +1,15 @@
 use rand::{distributions::Alphanumeric, Rng};
 use tracing::info;
 
-pub fn to_fixed(value: f64, decimals: i32) -> f64 {
+pub fn to_fixed(value: f64, decimals: u32) -> f64 {
     if decimals < 0 {
         info!("Value < 0");
         return value;
     }
-    let mut rounded = value * 10f64.powi(decimals.min(6)); // Scale by at most 10^6
-    rounded = rounded.round();
-    rounded / 10f64.powi(decimals.min(6))
+
+    let y = 10i32.pow(decimals) as f64;
+    (value * y).round() / y
+
 }
 
 pub fn random_string(len: usize) -> String {
